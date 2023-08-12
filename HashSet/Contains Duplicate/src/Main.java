33db033aafc335a0c599b9f421b309d2aab7ae62
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-12 14:56:48 +0530

    day 3

---
+++ HashSet/Contains Duplicate/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.containsDuplicate(new int[]{1,2,3,3,1}));
+    }
+}
