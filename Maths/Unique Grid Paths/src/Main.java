2439dd14d75b0024da8b80ac3d87ea8b06d3d4ee
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-12 10:00:14 +0530

    unique grid path

---
+++ Maths/Unique Grid Paths/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = solution.uniquePaths(3,7);
+        System.out.println(n);
+    }
+}
