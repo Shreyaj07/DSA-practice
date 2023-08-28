5610bd51a5337c3d7f382bd8b4f9f581f709ddc2
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-28 15:16:15 +0530

    afternoon dump

---
+++ Binary Search Tree/Implementation/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.populate(new int[]{15,2,7,1,6,9,8,3,10});
+        bst.display();
+    }
+}
