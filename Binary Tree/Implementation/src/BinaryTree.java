a475325449c568d6d2c0ab94383573c15bbb48c6
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-28 13:16:32 +0530

    binary tree

---
+++ Binary Tree/Implementation/src/BinaryTree.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.display();
+    }
+    private static class Node{
+        private int value;
+        Node left,right;
+        public Node(int value){
+            this.value = value;
+        }
+     }
+    private Node root;
+    public void populate(Scanner scanner){
+        System.out.println("Enter the root Node: ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner,root);
+    }
+    private void populate(Scanner scanner, Node node) {
+        System.out.println("Do you want to enter left of "+node.value);
+        boolean left = scanner.nextBoolean();
+        if(left){
+            System.out.println("Enter the value of left of "+node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner,node.left);
+        }
+        System.out.println("Do you want to enter right of "+node.value);
+        boolean right = scanner.nextBoolean();
+        if(right){
+            System.out.println("Enter the value of right of "+node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner,node.right);
+        }
+    }
+    public void display(){
+        display(root,"");
+    }
+    private void display(Node node, String indent) {
+        if(node==null){
+            return;
+        }
+        System.out.println(indent+node.value);
+        display(node.left,indent+"\t");
+        display(node.right,indent+"\t");
+    }
+}
---
+++ Linked List/LL/src/LL.java
@@ -0,0 +1,19 @@
+public class LL {
+
+    private Node head, tail;
+    private int size;
+    public LL(){
+        this.size =0 ;
+    }
+    private class Node{
+        private int value;
+        private Node next;
+        public Node(int value){
+            this.value = value;
+        }
+        public Node(int value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
