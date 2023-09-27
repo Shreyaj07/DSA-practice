a96d49a4403e813e0a1280cb66804fac752fee7a
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-28 00:47:28 +0530

    graph cycle

--- 21_Graph/Detect cycle in a directed graph/src/Main.java
+++ 21_Graph/Detect cycle in a directed graph/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<ArrayList<Integer>> edges= new ArrayList<>();
+        edges.add(new ArrayList<>(List.of(1,2)));
+        edges.add(new ArrayList<>(List.of(2,3)));
+        System.out.println(Solution.detectCycleInDirectedGraph(3,edges));
+        edges.add(new ArrayList<>(List.of(3,1)));
+        System.out.println(Solution.detectCycleInDirectedGraph(3, edges));
+
     }
 }
--- 21_Graph/Detect cycle in a directed graph/src/Solution.java
+++ 21_Graph/Detect cycle in a directed graph/src/Solution.java
@@ -1,6 +1,35 @@
 import java.util.ArrayList;
 public class Solution {
   public static boolean detectCycleInDirectedGraph(int n, ArrayList < ArrayList < Integer >> edges) {
-    // Write your code here.
+    ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+    for(int i=0; i<=n; i++){
+      adjList.add(i,new ArrayList<>());
+    }
+    for(int i=0; i<edges.size(); i++){
+      int u = edges.get(i).get(0);
+      int v = edges.get(i).get(1);
+      adjList.get(u).add(v);
+    }
+    boolean[] visited = new boolean[n+1];
+    boolean[] recursionStack = new boolean[n+1];
+    for(int i=1; i<=n; i++){
+      if(!visited[i] && hasCycleUtil(i,visited,recursionStack, adjList)){
+        return true;
+      }
+    }
+    return false;
+  }
+  public static boolean hasCycleUtil(int i, boolean[] visited, boolean[] recursionStack, ArrayList<ArrayList<Integer>> adjList){
+    visited[i] = true;
+    recursionStack[i] = true;
+    for(int neighbour: adjList.get(i)){
+      if(!visited[neighbour] && hasCycleUtil(neighbour, visited, recursionStack, adjList)){
+        return true;
+      }else if(recursionStack[neighbour]){
+        return true;
+      }
+    }
+    recursionStack[i] = false;
+    return false;
   }
 }
