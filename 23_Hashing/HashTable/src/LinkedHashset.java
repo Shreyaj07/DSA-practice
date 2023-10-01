0b9de97cd821bdcf3b9e4b8d80e273e7d0477e24
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-01 20:48:19 +0530

    updated
    Co-Authored-By: Shreya  Jadhav <109536426+Shreyaj07@users.noreply.github.com>
    Co-Authored-By: Sapna Kirad <89734847+SapnaKirad03@users.noreply.github.com>

--- 23_Hashing/HashTable/src/LinkedHashset.java
+++ 23_Hashing/HashTable/src/LinkedHashset.java
@@ -2,19 +2,19 @@ import java.util.LinkedHashSet;
 
 public class LinkedHashset {
     public static void main(String[] args) {
-        LinkedHashSet<String> lset= new LinkedHashSet<>();
+        LinkedHashSet<String> lset = new LinkedHashSet<>();
         lset.add("aaa");
         lset.add("bbb");
         lset.add("ccc");
         lset.add("ddd");
         System.out.println(lset);
-        System.out.println("Size: "+lset.size());
-        System.out.println("Check if ccc is present: "+lset.contains("ccc"));
-        System.out.println("Remove ddd"+lset.remove("ddd"));
+        System.out.println("Size: " + lset.size());
+        System.out.println("Check if ccc is present: " + lset.contains("ccc"));
+        System.out.println("Remove ddd" + lset.remove("ddd"));
         lset.add(null);
         System.out.println(lset);
         lset.add("ddd");
         System.out.println(lset);
-        
+
     }
 }
